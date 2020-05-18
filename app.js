587ee4d860196683d99ea50c90339992aa825a4e
Jacob <Jacobsutherland224@gmail.com>
2020-05-18 14:06:16 -0500

    added gallery functionality

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const dropDownMenu = document.querySelector('.drop-down');
 const exitIcon = document.querySelector('.close');
 const headerNavigation = document.querySelector('header nav');
 const navItems = document.querySelectorAll('header nav a');
+const imagesContainer = document.querySelector('.look-book-image-container');
+const images = document.querySelectorAll('.look-book-image-container div');
+const lookBookSection = document.querySelector('#look-book');
+const popupsSection = document.querySelector('#pop-ups');
+
 
 //Open hamburger menu
 dropDownMenu.addEventListener('click', () => {
@@ -19,4 +24,27 @@ for(let i = 0; i < navItems.length; i++){
         headerNavigation.classList.toggle('open-menu');
     });
 }
+//Creates 'X' icon to close image
+const closeImage = document.createElement('SPAN');
+closeImage.innerHTML =  'X';
+closeImage.classList.add('close-image')
+
+closeImage.addEventListener('click', () => {
+    for(let i = 0; i < images.length; i++){
+        images[i].classList.toggle('gallery-box');
+    }
+})
+
+
+for(let i = 0; i < images.length; i++){
+        images[i].addEventListener('click', () => {
+        images[i].classList.toggle('gallery-box');
+        imagesContainer.classList.toggle('hide-container');
+        lookBookSection.classList.toggle('reposition-pops');
+        popupsSection.classList.toggle('pop-up-margin');
+    })
+}
+
+
+
 
